3a28446421217735f41307d539243716e448b06b
Sophie Engle <sjengle@cs.usfca.edu>
2021-09-08 16:48:15 -0700

    Updated example

---
+++ ObjectOrientedProgramming/templates/PrefixMap.java
@@ -0,0 +1,20 @@
+public class PrefixMap {
+	// TODO Create members
+
+	public PrefixMap(int prefixSize) {
+		// TODO Fill in
+		throw new UnsupportedOperationException("Not yet implemented: " + prefixSize);
+	}
+
+	public PrefixMap() {
+		// TODO Fill in
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	public boolean addWord(String word) {
+		// TODO Fill in
+		throw new UnsupportedOperationException("Not yet implemented: " + word);
+	}
+
+	// TODO Add other methods
+}
