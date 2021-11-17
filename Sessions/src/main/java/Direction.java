630841d1c8e8dd5d55b4f6617efef1ab6cbc1a0e
Sophie Engle <sjengle@cs.usfca.edu>
2021-11-16 22:34:11 -0800

    Adding adventure game

---
+++ Sessions/src/main/java/Direction.java
@@ -0,0 +1,20 @@
+/**
+ * Represents the four cardinal directions. Points between the cardinal
+ * directions form the points of the compass.
+ *
+ * @author CS 272 Software Development (University of San Francisco)
+ * @version Fall 2021
+ */
+public enum Direction {
+	/** West cardinal direction. */
+	WEST,
+
+	/** North cardinal direction. */
+	NORTH,
+
+	/** East cardinal direction. */
+	EAST,
+
+	/** South cardinal direction. */
+	SOUTH;
+}
